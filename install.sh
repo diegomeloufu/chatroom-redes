93b9da1fa525517ebdd89892a8b0e0251751e281
diegomeloufu <diego-melo@ufu.br>
2024-07-25 22:31:35 -0300

    chore: Update Azure deployment workflow to use install.sh script for deployment

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ~/chatroom-redes
+git pull origin main
+sudo apt update
+sudo apt install -y python3-venv
+python3 -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+nohup python server.py 0.0.0.0 8000 > server.log 2>&1 &
