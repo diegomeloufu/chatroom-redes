7defda696ae4348149697bc567979f99b99ca4e3
diegomeloufu <diego-melo@ufu.br>
2024-07-25 22:44:45 -0300

    chore: Update install.sh script to kill process using port 8000 before starting server

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# Encerra o processo que está usando a porta 8000, se existir
+PORT=8000
+if lsof -Pi :$PORT -sTCP:LISTEN -t >/dev/null ; then
+    echo "Killing process running on port $PORT"
+    kill -9 $(lsof -Pi :$PORT -sTCP:LISTEN -t)
+fi
+
+# Atualiza e instala dependências
 sudo apt update
 sudo apt install -y python3-venv
 python3 -m venv venv
